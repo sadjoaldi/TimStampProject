2bd70d87e47ff616d96145d63ceda31b3f285df5
Sadjoaldi <alsannealdi@outlook.com>
2024-01-31 10:25:26 +0100

    inscription form done, beg style file

--- frontend/src/pages/Inscription.jsx
+++ frontend/src/pages/Inscription.jsx
@@ -3,8 +3,21 @@ import React from "react";
 function Inscription() {
   return (
     <>
-      <h1>je suis un formulaire d'inscription </h1>{" "}
-      <p>Page en cours de conception</p>
+      <form action="" htmlFor="">
+        <h1>TimeStamp </h1>
+        <label htmlFor="email">
+          EMAIL
+          <input type="email" name="email" id="email" />
+        </label>{" "}
+        <br />
+        <label htmlFor="password">
+          PASSWORD
+          <input type="password" name="password" id="password" />
+        </label>
+        <button type="button">Continue to signup</button>
+      </form>
+
+      <span>Log in to an existing account</span>
     </>
   );
 }
