d5f517a0e364eadbe292687cf50d6180eddbe12e
Sadjoaldi <alsannealdi@outlook.com>
2024-01-31 16:05:02 +0100

    inscription formular done

--- frontend/src/pages/Inscription.jsx
+++ frontend/src/pages/Inscription.jsx
@@ -1,24 +1,29 @@
 import React from "react";
+import "../styles/inscription.scss";
+import { Link } from "react-router-dom";
 
 function Inscription() {
   return (
-    <>
+    <div className="form">
       <form action="" htmlFor="">
         <h1>TimeStamp </h1>
-        <label htmlFor="email">
-          EMAIL
-          <input type="email" name="email" id="email" />
-        </label>{" "}
-        <br />
-        <label htmlFor="password">
-          PASSWORD
-          <input type="password" name="password" id="password" />
-        </label>
-        <button type="button">Continue to signup</button>
+        <label htmlFor="nom">Firstname</label>
+        <input type="text" name="nom" id="nom" placeholder="*" />
+        <label htmlFor="nom">Lastname</label>
+        <input type="text" name="nom" id="nom" placeholder="*" />
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" placeholder="*" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" placeholder="*" />
+        <label htmlFor="password">Confirm password</label>
+        <input type="password" name="password" id="password" placeholder="*" />
+        <button type="button">Continue to signup</button>{" "}
+        <span>
+          {" "}
+          <Link to="/">Log in to an existing account</Link>
+        </span>
       </form>
-
-      <span>Log in to an existing account</span>
-    </>
+    </div>
   );
 }
 
