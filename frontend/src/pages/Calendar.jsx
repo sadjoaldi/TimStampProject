2567b4614edb513ffa3e3481a499bb39a3b3d20e
Sadjoaldi <alsannealdi@outlook.com>
2024-02-08 01:37:11 +0100

    dashboard in place, some settings later

--- frontend/src/pages/Calendar.jsx
+++ frontend/src/pages/Calendar.jsx
@@ -1,11 +1,66 @@
-import React from "react";
+// import React, { useState } from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+// import { IoIosCalendar } from "react-icons/io";
 import "../styles/calendar.scss";
+import { useEffect, useRef, useState } from "react";
 
 function Calendar() {
+  // const [events, setEvents] = useState([]);
+
+  const calendarRef = useRef(null);
+
+  const [projectName, setProjectName] = useState("");
+
+  const handlKeyDown = (e) => {
+    if (e.key === 13) {
+      // console.info("hello");
+    }
+  };
+
+  useEffect(() => {
+    const calendarApi = calendarRef.current.getApi();
+    const dateInput = document.getElementById("dateInput");
+
+    dateInput.addEventListener("change", () => {
+      const newDate = new Date(calendarRef);
+      calendarApi.gotoDate(newDate);
+    });
+  }, []);
   return (
-    <div>
-      <h1>je suis un calendar</h1>
-    </div>
+    <>
+      <div className="project_name">
+        <input
+          type="text"
+          name="project_name"
+          value={projectName}
+          placeholder="Lieu de travail"
+          id="project_name"
+          onChange={(e) => setProjectName(e.target.value)}
+          onKeyDown={handlKeyDown}
+        />
+
+        <input type="text" id="dateInput" placeholder="hello" />
+      </div>
+      <div className="calendar">
+        <div className="calendar_box">
+          <FullCalendar
+            plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+            ref={calendarRef}
+            headerToolbar={{
+              start: "today prev next",
+              center: "title",
+              right: "timeGridWeek,timeGridDay,dayGridMonth",
+            }}
+            allDaySlot={false}
+            contentHeight="auto"
+          />
+          ;
+        </div>
+      </div>
+    </>
   );
 }
 
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,13 +1,19 @@
 import React from "react";
-// import { Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import Calendar from "./Calendar";
+import "../styles/dashboard.scss";
 
 function Dashboard() {
   return (
-    <div>
-      <Sidebar />
-      <Calendar />
+    <div className="dash_container">
+      <div className="side">
+        <Sidebar />
+      </div>
+      <div className="dashBox">
+        <Outlet />
+        <Calendar />
+      </div>
     </div>
   );
 }
